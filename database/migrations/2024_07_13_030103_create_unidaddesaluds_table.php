42cc026d4834d5753b8ff754824c5431c3383d80
JoseAg87 <a22100039@unideh.edu.mx>
2024-08-02 18:55:03 -0600

    BD cambios

--- database/migrations/2024_07_13_030103_create_unidaddesaluds_table.php
+++ database/migrations/2024_07_13_030103_create_unidaddesaluds_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('unidaddesaluds', function (Blueprint $table) {
+        Schema::create('unidaddesalud', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('IdUnidad');
             $table->string('Nombre');
@@ -30,6 +30,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('unidaddesaluds');
+        Schema::dropIfExists('unidaddesalud');
     }
 };
